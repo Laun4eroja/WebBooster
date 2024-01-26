165bdf4a0dfff7bd98394ff0b36578abb69f56ad
Valery Biktimirov <Biktimirov93@mail.ru>
2024-01-26 19:26:43 +0300

    section 1 and section 2 ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 $(".header__intup-tel").mask("+7(999)999-9999");
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 18,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.btn-next',
+      prevEl: '.btn-prev',
+    },
+  });
